1445b47e8c47bc28bbe2434310fd16f22d37bf90
mrHoft <ru.hoft@gmail.com>
2025-05-26 21:18:13 +0300

    feat: interfaces

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+const PORT = parseInt(process.env.PORT || '4000', 10);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  await app.listen(PORT);
 }
 bootstrap();
